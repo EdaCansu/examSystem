bf203152175d2cf7fb89c1cfc33e03c0eaa6f252
EdaCansu <edamiaj@gmail.com>
2023-01-29 20:27:19 +0300

    index & app

--- frontend/exam/src/App.js
+++ frontend/exam/src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import ListQuestion from './component/exam/ListQuestion';
+import HomePage from './component/exam/HomePage';
+import UpdateQuestion from './component/exam/UpdateQuestion';
+import ViewQuestion from './component/exam/ViewQuestion';
+import UserPage from './component/exam/UserPage';
+import Header from './component/Header';
+import Footer from './component/Footer';
+import ExamPage from './component/exam/ExamPage';
+import ExamEnd from './component/exam/ExamEnd';
+import AddQuestion from './component/exam/AddQuestion';
+import SubmitUpdateQuestion from './component/exam/SubmitUpdateQuestion';
+import SubmitAddQuestion from './component/exam/SubmitAddQuestion';
+
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+
+      <Router>
+        <Switch>
+          <Route path="/" exact component={HomePage}></Route>
+          <Route path="/home" component={HomePage}></Route>
+          <div>
+            <Header/>
+            <Route path="/list-question" component={ListQuestion}></Route>
+            <Route path="/add-question" component={AddQuestion}></Route>
+            <Route path="/update-question/:id" component={UpdateQuestion}></Route>
+            <Route path="/view-question/:id" component={ViewQuestion}></Route>
+            <Route path="/added" component={SubmitAddQuestion}></Route>
+            <Route path="/updated" component={SubmitUpdateQuestion}></Route>
+            <Route path="/user" component={UserPage}></Route>
+            <Route path="/exam" component={ExamPage}></Route>
+            <Route path="/end" component={ExamEnd}></Route>
+            <Footer/>
+          </div>
+        </Switch>
+      </Router>
+    </>
   );
 }
 
